905a50d24550da76a7824fdd0d1638fbbd78a88b
Somniii <somnii006@gmail.com>
2026-07-23 20:12:16 -0300

    build recigram

---
+++ backend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex">
+      <h1 className="text-color-neutral">Welcome to recipy!!</h1>
+    </div>
+  );
+}
